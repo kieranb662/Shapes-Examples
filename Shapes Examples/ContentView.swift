714c69397a1f81d1d7c73a8e6809e565bfc9ec9c
Kieran Brown <kb6627500@gmail.com>
2020-04-14 09:47:01 -0400

    Initial Commit

---
+++ Shapes Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shapes Examples
+//
+//  Created by Kieran Brown on 4/14/20.
+//  Copyright © 2020 BrownandSons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
